1869b7a0bfb537feb2fce538c4a36b935c50757b
Bhrikuty Aggarwal <bhrikuty.aggarwal@gmail.com>
2021-02-25 20:11:14 +0530

    Update node.go

--- cmd/node.go
+++ cmd/node.go
@@ -36,7 +36,8 @@ var nodeCmd = &cobra.Command{
 		filename := loadConfig(check.NODE, bv)
 		runChecks(check.NODE, filename)
 		writeOutput(controlsCollection)
-	}
+	},
+	Deprecated: "this command will be retired soon. Please use the `run` command with `--targets=node` instead.",
 }
 
 func init() {
